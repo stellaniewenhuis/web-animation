e7f3a4a3aac907138cd799b19cc8f4d5848de372
stellaniewenhuis <45544342+stellaniewenhuis@users.noreply.github.com>
2020-05-25 09:40:07 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-console.log("load javascript");
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+
+//standaard achtergrondkleur instelling
 document.body.style.backgroundColor = "white";
+
+//variabelen
 var base = document.getElementById("art");
-
 var points = document.getElementsByClassName('st2');
-console.log("found points:" + points.length);
 
+//for loop : voegt event listener toe aan alle bolletjes
 for (var i = 0; i < points.length; i++) {
     points[i].addEventListener('click', clickEvent);
 }
 
+//toggle functie
 function clickEvent() {
     if (this.classList.contains("clicked")) {
         this.classList.remove("clicked");
@@ -18,6 +25,7 @@ function clickEvent() {
     }
 }
 
+//Event listeners op de cijfers in het kunstwerk met daarachter de functie die uitgevoerd wordt als er geklikt wordt
 document.getElementById("c1").addEventListener('click', function() {
     random_dots(1);
 });
@@ -30,6 +38,8 @@ document.getElementById("c3").addEventListener('click', function() {
 document.getElementById("c4").addEventListener('click', function() {
     random_dots(4);
 });
+
+//Event listeners op de buttons onder het kunstwerk met daarachter de functie die uitgevoerd wordt als er geklikt wordt
 document.getElementById("rotate_button").addEventListener('click', rotate);
 document.getElementById("float_button").addEventListener('click', float);
 document.getElementById("shake_button").addEventListener('click', shake);
@@ -38,25 +48,24 @@ document.getElementById("background_button").addEventListener('click', backgroun
 
 document.getElementById("een_button").addEventListener('click', highlight_1);
 document.getElementById("twee_button").addEventListener('click', highlight_2);
+document.getElementById("drie_button").addEventListener('click', highlight_3);
+document.getElementById("vier_button").addEventListener('click', highlight_4);
 
-
+//zorgt dat dingen gebeuren wanneer een toets wordt ingedrukt
 document.addEventListener('keypress', function(e) {
-    console.log("key clicked: " + e.keyCode)
     switch (e.keyCode) {
-        case 55: random_dots(1); break;
-        case 56: random_dots(2); break;
-        case 57: random_dots(3); break;
-        case 58: random_dots(4); break;
-        case 53: clear_dots(); break;
-        case 54: background_change(); break;
+        case 53: random_dots(1); break;
+        case 54: random_dots(2); break;
+        case 55: random_dots(3); break;
+        case 56: random_dots(4); break;
         case 49: highlight_1(); break;
         case 50: highlight_2(); break;
         case 51: highlight_3(); break;
         case 52: highlight_4(); break;
-
     }
 });
 
+//de class clicked weghalen van alle bolletjes
 function clear_dots() {
     var clicked = document.getElementsByClassName("clicked");
     while(clicked.length > 0) {
@@ -64,6 +73,7 @@ function clear_dots() {
     }
 }
 
+//functie achtergrond veranderen
 function background_change() {
     if (document.body.style.backgroundColor == "white") {
         document.body.style.backgroundColor = "black";
@@ -73,20 +83,22 @@ function background_change() {
     }
 }
 
+//random bolletjes pakken uit het totaal aantal bolletjes
 function getRandomInt() {
   var min = 0;
   var max = points.length;
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+
 function random_dots(num) {
-    // set all points to unclicked
+    // alle bolletjes unclicked maken
     clear_dots();
 
-    // make empty list of length num
+    // een lege lijst van num maken
     var idx = new Array(num);
 
-    // fill list with random indices
+    // de lijst vullen met random integers
     for (i = 0; i < num; i++) {
         while (true) {
             var new_num = getRandomInt();
@@ -103,6 +115,7 @@ function random_dots(num) {
 }
 
 
+//toggle functie
 function flip () {
     if (base.classList.contains("flip")) {
         base.classList.remove("flip");
@@ -113,6 +126,7 @@ function flip () {
     }
 }
 
+//toggle functie
 function shake () {
     if (base.classList.contains("shake")) {
         base.classList.remove("shake");
@@ -123,6 +137,7 @@ function shake () {
     }
 }
 
+//toggle functie
 function rotate() {
     if (base.classList.contains("rotate")) {
         base.classList.remove("rotate");
@@ -133,6 +148,7 @@ function rotate() {
     }
 }
 
+//toggle functie
 function float() {
     if (base.classList.contains("float")) {
         base.classList.remove("float");
@@ -143,7 +159,7 @@ function float() {
     }
 }
 
-
+//functie van het highlighten van de bolletjes en streepjes en kromme streepjes
 function highlight(id, type) {
 
     var object = document.getElementById(id);
@@ -178,12 +194,14 @@ function highlight_1() {
     clear_highlights();
     clear_dots();
 
+    // highlight bolletjes
     highlight("bolletje_26_1_", "st2");
     highlight("bolletje_25_1_", "st2");
     highlight("bolletje_24_1_", "st2");
     highlight("bolletje_23", "st2");
     highlight("bolletje_26", "st2");
 
+    // highlight streepjes
     highlight("streepje_21", "st1");
     highlight("streepje_22", "st1");
     highlight("streepje_23", "st1");
@@ -201,7 +219,7 @@ function highlight_2() {
     clear_highlights();
     clear_dots();
 
-    // highlight dots
+    // highlight bolletjes
     highlight("bolletje_16", "st2");
     highlight("bolletje_17", "st2");
     highlight("bolletje_18", "st2");
@@ -214,7 +232,7 @@ function highlight_2() {
     highlight("streepje_18", "st1");
     highlight("streepje_20", "st1");
 
-    // highligh kromme streep
+    // highligh kromme streepjes
     highlight("streepje_1_", "st0");
     highlight("streepje_17_1_", "st0");
 }
@@ -229,7 +247,7 @@ function highlight_3() {
     clear_highlights();
     clear_dots();
 
-    // highlight dots
+    // highlight bolletjes
     highlight("bolletje_15", "st2");
     highlight("bolletje_14", "st2");
     highlight("bolletje_13", "st2");
@@ -247,14 +265,14 @@ function highlight_3() {
     highlight("streepje_11", "st1");
     highlight("streepje_8",  "st1");
 
-    // highligh kromme streep
+    // highligh kromme streepjes
     highlight("streepje_10", "st0");
     highlight("streepje_9", "st0");
 
 }
 
 function highlight_4() {
-    var object = document.getElementById("bolletje_26_1_")
+    var object = document.getElementById("bolletje_1")
     if (object.getElementsByClassName("st2")[0].classList.contains("st2-highlight")) {
         clear_highlights();
         return
@@ -263,7 +281,7 @@ function highlight_4() {
     clear_highlights();
     clear_dots();
 
-    // highlight dots
+    // highlight bolletjes
     highlight("bolletje_1", "st2");
     highlight("bolletje_2", "st2");
     highlight("bolletje_3", "st2");
@@ -284,14 +302,19 @@ function highlight_4() {
 
 
 
-
-
-
-
-
-
-
-
+//https://stackoverflow.com/questions/256754/how-to-pass-arguments-to-addeventlistener-listener-function
+//https://stackoverflow.com/questions/4825295/javascript-onclick-to-get-the-id-of-the-clicked-button
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+//https://stackoverflow.com/questions/10139588/check-for-false
+//https://stackoverflow.com/questions/3390396/how-to-check-for-undefined-in-javascript
+//shttps://www.w3schools.com/js/js_switch.asp
+//https://stackoverflow.com/questions/17015019/keylistener-in-javascript
+//http://gcctech.org/csc/javascript/javascript_keycodes.htm
+//https://www.w3schools.com/jsref/event_onclick.asp
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onclick_addeventlistener
+//https://stackoverflow.com/questions/9882284/looping-through-array-and-removing-items-without-breaking-for-loop
+//https://stackoverflow.com/questions/5597060/detecting-arrow-key-presses-in-javascript
+//https://stackoverflow.com/questions/197748/how-do-i-change-the-background-color-with-javascript
 
 
 
